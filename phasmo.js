218e0bf550ae360ade2a20a9bba9e5a6d1497757
markskigaming <markskigamingtwitch@gmail.com>
2021-01-05 20:28:27 +0100

    made it a option to pog instead

--- phasmo.js
+++ phasmo.js
@@ -1,4 +1,13 @@
 function doNotPog() {
-    alert("not pog ;-;");
+    alert("( not pog )> ;-;");
+    let isIt = prompt("does it work now? yes or no");
+    switch(isIt) {
+        case "yes":
+            alert("i take it back then! POG");
+        case "no":
+            alert(";-; <(not pog)");
+        default:
+            alert("oi i said yes or no not 'siri give me a fun fact'");
+    }
 }
 doNotPog();
