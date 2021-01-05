9ea6f022331760e2cd2750ea902ee592100ad6e4
markskigaming <markskigamingtwitch@gmail.com>
2021-01-05 22:32:35 +0100

    made it so the code sees when a checkbox is checked

--- phasmo.js
+++ phasmo.js
@@ -1,13 +1,82 @@
-function doNotPog() {
-    alert("( not pog )> ;-;");
-    let isIt = prompt("does it work now? yes or no");
-    switch(isIt) {
-        case "yes":
-            alert("i take it back then! POG");
-        case "no":
-            alert(";-; <(not pog)");
-        default:
-            alert("oi i said yes or no not 'siri give me a fun fact'");
+function getUserClues() {
+    let numClues = +prompt("How many clues do you have?");
+    let userClues = [];
+  
+    for(let i = 0; i < numClues; i++) {
+        let rC = prompt(`What is clue #${i+1}?`);
+        let clue = translateUserClue(rC);
+        userClues.push(clue);
+        
     }
+    return userClues;
 }
-doNotPog();
+
+function getClues() {
+    let emf5Button = false;
+    let tempButton = document.getElementById("freezingTemp").checked;
+    let cls = [];
+    if(emf5Button == true) {
+        cls.push(EMF_LEVEL_5);
+    } else if (tempButton == true){
+        cls.push(FREEZING_TEMPERATURES);
+    }
+    return cls;
+}
+
+function freezingTempsClick() {
+    
+}
+
+function getMissingClues(ghostClues, userClues) {
+    let missingC = [];
+    for(c = 0; c<ghostClues.length; c++) {
+        if (!userClues.includes (ghostClues[c])) {
+            missingC.push(ghostClues[c]);
+        }
+    }
+    return missingC;
+}
+
+function couldBeGhost(ghostClues, userClues){
+    for(let i = 0; i < userClues.length; i++ ) {
+        let includesClue = ghostClues.includes(userClues[i]);
+        if(!includesClue) {
+            return false;
+        }
+    } 
+    return true;
+}
+
+function getMissingClues(ghostClues, userClues){
+    let missingClues = [];
+    for(let q = 0; q < ghostClues.length; q++){
+        if(!userClues.includes(ghostClues[q])){
+            missingClues.push(ghostClues[q]);
+        }
+    } return missingClues;
+}
+
+function runPhasmoScript(){
+    // let userClues = getUserClues();
+    let userClues = getClues();
+
+    let ghosts = getGhostList();
+    ghosts.forEach(function(ghost){
+        let ghostName = ghost.name;
+        let ghostClues = ghost.clues;
+
+        let couldBe = couldBeGhost(ghostClues, userClues);
+        if(couldBe) {
+            let missingClues = getMissingClues(ghostClues, userClues);
+            console.log(`${ghostName}: [${missingClues.join(", ")}]`);
+        }
+    });
+}
+
+var freezingCheckbox = document.querySelector("input[id=freezingTemp]");
+
+freezingCheckbox.addEventListener('change', function() {
+    runPhasmoScript();
+});
+
+runPhasmoScript();
