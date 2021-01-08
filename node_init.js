ba7839cbf48d05e0edcc7f5908ea96860c91dd1b
Vin Turtle Dig <VinTurtleDig@gmail.com>
2021-01-08 11:48:22 -0500

    Add node setup for phasmo tool.

---
+++ node_init.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+
+// const express = require('express');
+// const app = express();
+// app.use(express.static('./'));
+
+http.createServer(function (req, res) {
+    let path = "phasmo.html";
+    if(req.url != "/") {
+        path = req.url.substring(1, req.url.length);
+    }
+    fs.readFile(path, function (err, data) {
+        if(data === undefined) {
+            return res.end();
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(process.env.PORT || 5000);
